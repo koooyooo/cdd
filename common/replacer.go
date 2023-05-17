bcc5da5a70ac17798d9455896a0c1e80fdad7855
koooyooo <jab.cross.hook@gmail.com>
2023-05-17 13:02:27 +0900

    apply rel path

--- common/replacer.go
+++ common/replacer.go
@@ -1,28 +1,59 @@
 package common
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
+type dirFunc func() (string, error)
+type dirCheckFunc func(string) error
+
 func Replace4Store(path string) (string, error) {
+	return _replace4store(
+		path,
+		func() (string, error) { return os.UserHomeDir() },
+		func() (string, error) { return os.Getwd() },
+		func(path string) error {
+			if !Exists(path) {
+				return fmt.Errorf("no dir found for path: [%s]\n", path)
+			}
+			return nil
+		},
+	)
+}
+
+func _replace4store(path string, hdF, wdF dirFunc, chF dirCheckFunc) (string, error) {
+	hd, err := hdF()
+	if err != nil {
+		return "", err
+	}
+	wd, err := wdF()
+	if err != nil {
+		return "", err
+	}
 	if path == "." {
-		wd, err := os.Getwd()
-		if err != nil {
-			return "", err
-		}
-		hd, err := os.UserHomeDir()
-		if err != nil {
-			return "", err
-		}
-		path = wd
-		if strings.HasPrefix(path, hd) {
-			path = filepath.Join("${HOME}", strings.TrimPrefix(path, hd))
-		}
+		return replaceHome(wd, hd)
+	}
+	if path == "$HOME" || path == "${HOME}" {
 		return path, nil
 	}
+	if isRelPath := !strings.HasPrefix(path, "/"); isRelPath {
+		joinedPath := filepath.Join(wd, path)
+		if err := chF(joinedPath); err != nil {
+			return "", err
+		}
+		return replaceHome(joinedPath, hd)
+	}
+	return replaceHome(path, hd)
+}
 
+func replaceHome(path, homeDir string) (string, error) {
+	if isLeafOfHD := strings.HasPrefix(path, homeDir); isLeafOfHD {
+		shortenedPath := filepath.Join("${HOME}", strings.TrimPrefix(path, homeDir))
+		return shortenedPath, nil
+	}
 	return path, nil
 }
 
